edd821d868b0e1be918eb7f78e6c2ae88cc94656
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2022-10-20 15:16:14 +0300

    search

--- web/app/themes/ula-theme/search.php
+++ web/app/themes/ula-theme/search.php
@@ -9,10 +9,26 @@
  * @since   Timber 0.1
  */
 
-$templates = array( 'search.twig', 'archive.twig', 'index.twig' );
+//$templates = array( 'search.twig', 'archive.twig', 'index.twig' );
+//
+//$context          = Timber::context();
+//$context['title'] = 'Search results for ' . get_search_query();
+//$context['posts'] = new Timber\PostQuery();
+//
+//Timber::render( $templates, $context );
 
-$context          = Timber::context();
-$context['title'] = 'Search results for ' . get_search_query();
-$context['posts'] = new Timber\PostQuery();
 
-Timber::render( $templates, $context );
+use Timber\Timber;
+
+defined( 'ABSPATH' ) || exit;
+
+global $wp_query;
+
+$context = new Ula\Timber\Context( ['search.twig'] );
+
+$context->add( 'search', [
+    'query' => get_search_query(),
+    'count' => $wp_query->found_posts
+] );
+
+$context->render();
